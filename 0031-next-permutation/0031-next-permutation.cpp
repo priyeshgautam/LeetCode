549fa3da17d17017036a4002baaf5fb013c2869a
Priyesh Gautam <66623731+priyeshgautam@users.noreply.github.com>
2025-06-07 14:38:57 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int size = nums.size();
-        int index = -1;
-        for(int i = size-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                index = i;
-                break;
-            }
+void nextPermutation(vector<int>& nums) {
+    int size = nums.size();
+    int index = -1;
+    // nums = 1 5 4 3 2
+    // Step 1: Find the first index from the end where nums[i] < nums[i+1]
+    for (int i = size - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            index = i;
+            break;
         }
-        if(index != -1) {
-            for(int i = size-1 ;i>index;i--){
-            if(nums[i] > nums[index]){
+    }
+    // index = 0
+
+    // Step 2: If such an index exists, find next greater element and swap
+    if (index != -1) {
+        for (int i = size - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
                 swap(nums[i], nums[index]);
                 break;
             }
         }
     }
-        
-        reverse(nums.begin()+index+1, nums.end() );
-    }
+    // 
+
+    // Step 3: Reverse the suffix (from index+1 to end)
+    reverse(nums.begin() + index + 1, nums.end());
+}
+
+    // void nextPermutation(vector<int>& nums){
+    //     int size = nums.size();
+    //     int index = -1;
+    //     for(int i =size-2; i>=0; i--){
+    //         if(nums[i+1]>nums[i]){
+    //             index = i;
+    //             break;
+    //         }
+    //     }
+    //     if(index != -1){
+    //         for(int i =size-1; i>index; i--){
+    //             if(nums[i]<nums[index]){
+    //                 swap(nums[i],nums[index]);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     reverse(nums.begin()+index+1, nums.end());
+    // }
 };
